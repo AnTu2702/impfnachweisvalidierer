2abc9c37d5c3acf3b14c49900597665dd508299f
Andreas Tully <andreas.tully@deutschebahn.com>
2021-06-05 23:38:02 +0200

    Update

--- hc1_verify.py
+++ hc1_verify.py
@@ -42,17 +42,17 @@ class HC1Verify:
 
         return cose.verify_signature()
 
-    def print(self, result):
+    def show(self, result):
 
         print(f"\r\nDecoding and validating your token with given certificate...")
-        print(f"--------------------------------------------------------------------------------------------------------")
+        print(f"--------------------------------------------------------------------------------------------------")
         print(json.dumps(self.health, indent=4, sort_keys=True, ensure_ascii=False))
-        print(f"--------------------------------------------------------------------------------------------------------")
+        print(f"--------------------------------------------------------------------------------------------------")
         print(f"Issuer: {self.issuer}")
         print(f"Issued At: {datetime.datetime.utcfromtimestamp(self.issued).strftime('%d.%m.%Y, %H:%M:%S')}")
         print(f"Experation time: {datetime.datetime.utcfromtimestamp(self.expires).strftime('%d.%m.%Y, %H:%M:%S')}")
         print(f"Is valid: {result} - Validation Key: {self.keyid}")
-        print(f"--------------------------------------------------------------------------------------------------------")
+        print(f"--------------------------------------------------------------------------------------------------")
 
 @click.command()
 @click.option('-c', '--cert', type=str, required=True, default='./demo-dsc.crt', help="da cert...")
@@ -64,7 +64,7 @@ def main(cert, token):
         hc1 = HC1Verify()
         hc1.load(cert)
         result = hc1.verify(token)
-        hc1.print(result)
+        hc1.show(result)
 
     except Exception as exc:
         e_tp, e_vl, e_tb = sys.exc_info()
