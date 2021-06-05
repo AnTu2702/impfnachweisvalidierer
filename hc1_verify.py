29185da7f530f243c344749fab4df6cce998a71f
Andreas Tully <andreas.tully@deutschebahn.com>
2021-06-05 22:48:36 +0200

    Update

--- hc1_verify.py
+++ hc1_verify.py
@@ -44,14 +44,14 @@ class HC1Verify:
             decompressed = zlib.decompress(compressed)
             cose = CoseMessage.decode(decompressed)
             cose.key = CoseKey.from_dict({KpKty: KtyEC2, EC2KpCurve: P256, KpAlg: Es256, EC2KpX: self.key_x, EC2KpY: self.key_y})
-            jsondata = cbor2.loads(cose.payload)
+            cbor = cbor2.loads(cose.payload)
             
             claims = { "Issuer" : 1, "Issued At" : 6, "Experation time" : 4, "Health claims" : -260 }
 
-            health = jsondata[claims["Health claims"]]
-            issuer = jsondata[claims["Issuer"]]
-            issued = jsondata[claims["Issued At"]]
-            expires = jsondata[claims["Experation time"]]
+            health = cbor[claims["Health claims"]]
+            issuer = cbor[claims["Issuer"]]
+            issued = cbor[claims["Issued At"]]
+            expires = cbor[claims["Experation time"]]
 
             print(f"\r\nDecoding and validating your token with given certificate...")
             print(f"---------------------------------------------------------------------------------------------")
