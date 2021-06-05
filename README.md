eaadff5460fd9fd19c4aff941010c27b7a087350
Andreas Tully <andreas.tully@googlemail.com>
2021-06-05 19:42:44 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,51 @@
+# Trivial/rudimentary eHN-simplified implementation
+
+Aligned with version 1.00 / 2021-4-14 of the spec:
+	https://github.com/ehn-digital-green-development/hcert-spec/blob/main/hcert_spec.md
+
+For round-trip testing of ```hc1_sign.py``` and ```hc1_verify.py``` take some
+JSON, e.g. ```{ "Foo" : "Bar }```, CBOR package, COSE sign, compress and base45
+convert it for use in a QR.
+
+1. COSE sign
+   1. compact the JSON into CBOR
+   1. wrap it into a payload (Health claim -260, add issuer/dates)
+   1. sign and package as a COSE message
+   1. ZLIB compress
+   1. Base45 encode 
+1. COSE verify     
+   1. Base45 decode
+   1. ZLIB decompress
+   1. check the signature on the COSE message
+   1. unpack the CBOR into JSON
+   1. unpack the payload and extract the issuer and dates
+   1. unpack the health claim and output as json.
+
+### Test Steps
+
+1. Generate the CSCA and DSC with ```./gen-csca-dsc.sh```	
+1. Run the command: ```echo "{'A': 1234}" | python3.8 hc1_sign.py | python3.8 hc1_verify.py```
+1. You should see the output: ```{"A": 1234}```
+
+```echo '{ "Foo":1, "Bar":{ "Field1": "a value",   "integer":1212112121 }}' | python3.8 hc1_sign.py | python3.8 hc1_verify.py prettyprint-json```
+
+Which should output:
+
+```
+{
+    "Foo": 1, 
+    "Bar": {
+        "Field1": "a value", 
+        "integer": 1212112121
+   }
+}
+```
+
+# Testing COSE from Austrian website
+
+Testing against the AT cases:
+
+1. Fetch the Base64 from https://dev.a-sit.at/certservice
+1. Remove the first 2 bytes and do
+
+   ```pbpaste| sed -e 's/^00//' | python3.8 hc1_verify.py --base64 --ignore-signature --cbor```
---
+++ hc1_verify.py
@@ -0,0 +1,91 @@
+import argparse, json, sys, zlib, click, cbor2, datetime
+
+from base45 import b45decode
+from cose.algorithms import Es256
+from cose.curves import P256
+from cose.algorithms import Es256, EdDSA
+from cose.headers import KID
+from cose.keys import CoseKey
+from cose.keys.keyparam import KpAlg, EC2KpX, EC2KpY, EC2KpCurve
+from cose.keys.keyparam import KpKty
+from cose.keys.keytype import KtyEC2
+from cose.messages import CoseMessage
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+
+class HC1Verify:
+
+    def __init__(self):
+
+        self.key_x = None
+        self.key_y = None
+
+    def _json_serial(self, obj):
+
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        raise TypeError ("Type %s not serializable" % type(obj))
+
+    def load(self, cert):
+
+        with open(cert, "rb") as file:
+            pem = file.read()
+
+            cert = x509.load_pem_x509_certificate(pem)
+            pub = cert.public_key().public_numbers()
+
+            fingerprint = cert.fingerprint(hashes.SHA256())
+            keyid = fingerprint[0:8]
+
+            self.key_x = pub.x.to_bytes(32, byteorder="big")
+            self.key_y = pub.y.to_bytes(32, byteorder="big")
+
+    def verify(self, token):
+
+            compressed = b45decode(token.decode('utf-8'))
+            decompressed = zlib.decompress(compressed)
+            decoded = CoseMessage.decode(decompressed)
+            decoded.key = CoseKey.from_dict({KpKty: KtyEC2, EC2KpCurve: P256, KpAlg: Es256, EC2KpX: self.key_x, EC2KpY: self.key_y})
+            
+            claims = { "Issuer" : 1, "Issued At" : 6, "Experation time" : 4, "Health claims" : -260 }
+            
+            payload = cbor2.loads(decoded.payload)
+
+            health = payload[claims["Health claims"]]
+            issuer = payload[claims["Issuer"]]
+            issued = payload[claims["Issued At"]]
+            expires = payload[claims["Experation time"]]
+
+            print(f"\r\nDecoding und Validating your token with given certificate...")
+            health_json = json.dumps(health, indent=4, sort_keys=True, default=self._json_serial)
+            print(f"---------------------------------------------------------------------------------------------")
+            print(health_json)
+            print(f"---------------------------------------------------------------------------------------------")
+            print(f"Issuer: {issuer}")
+            print(f"Issued At: {datetime.datetime.utcfromtimestamp(issued).strftime('%d.%m.%Y, %H:%M:%S')}")
+            print(f"Experation time: {datetime.datetime.utcfromtimestamp(expires).strftime('%d.%m.%Y, %H:%M:%S')}")
+            print(f"Is valid: {decoded.verify_signature()}")
+            print(f"---------------------------------------------------------------------------------------------")
+
+@click.command()
+@click.option('-c', '--cert', type=str, required=True, default='./demo-dsc.crt', help="da crt")
+@click.option('-t', '--token', type=str, required=True, help="da token")
+
+def main(cert, token):
+
+    try:
+        hc1 = HC1Verify()
+        hc1.load(cert)
+        hc1.verify(token.encode('utf-8'))
+
+    except Exception as exc:
+        e_tp, e_vl, e_tb = sys.exc_info()
+        print(f"[Failure] - Exiting with Critical Error: {e_tp, e_tb.tb_frame.f_code.co_filename, e_tb.tb_lineno, e_vl} - Aborting.")
+        print(f"[Exception] - Reason: {exc}.")
+
+    finally:
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
